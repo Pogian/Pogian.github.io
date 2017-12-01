49f82a81f856d662c622f5b454ead07e233f4692
Mekikwan <mekikwan@gmail.com>
2017-11-30 18:12:39 -0800

    v11

--- jquery.js
+++ jquery.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
-	$("img#hamburgerIcon").click(function(){
-		$(".main li").stop().slideToggle();
-	});
-	if($(window).width() >= 375){
-		alert("this is 375");
-	}
-
-
+	
 	/*---------------function for videos---------------*/
 	$("div.episodeWrapper_Text li a").click(function(e){
 		e.preventDefault();
@@ -24,18 +17,10 @@ $(document).ready(function(){
 	});
 	/* ---------------end of function for videos---------------*/
 
-	$(".main li").hover(function(){
-		$(this).find(".submenu li").stop().slideToggle("fast");	
-	});
-	$("img").tooltip({track: true});
 	/* ---------------end of function for Navigator---------------*/
 
 	$(".contentWrapper_Text li").click(function(){
 		$("div .story").hide();
 	});
-
-
-
-
 
 });
