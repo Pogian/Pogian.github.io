60fc5a26d441e883d97d1f8111023c9f50b18172
Mekikwan <mekikwan@gmail.com>
2017-12-01 17:00:48 -0800

    v16

--- jquery.js
+++ jquery.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 	
-	/*---------------slider for images---------------*/
-	 
+
 	/*---------------function for videos---------------*/
 	$("div.episodeWrapper_Text li a").click(function(e){
 		e.preventDefault();
 		$(".animeVideoPage").show();
+		$("#disqus_thread").show();
 		$(".animeFrontPage").hide();
 	$("#videoPlayer").attr("src", $(this).attr("href"));
 	$("#videoPlayer").attr("title", $(this).attr("title"));
@@ -26,3 +26,24 @@ $(document).ready(function(){
 	});
 
 });
+
+/*---------------Start of search function---------------*/
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('searchBox_Input');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+/*---------------end of search function---------------*/
